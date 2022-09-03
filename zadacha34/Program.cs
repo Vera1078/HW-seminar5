aed1d9a3fd0f607db7d34aa0b02df07aa5a827fe
Vera Lippgardt <vera1078@mail.ru>
2022-09-03 10:28:27 +0300

    Количество четных элементов в случайно заданном массиве

---
+++ zadacha34/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] RandomArray (int number)
+{
+
+int[] array = new int[number];
+
+    for (int i = 0; i < array.Length; i++)
+    {           
+        array[i] = new Random().Next(100, 1000);        
+    }
+        Console.WriteLine($"Массив: {String.Join(", ", array)}");
+    
+    return array;
+ }
+
+ void CounterOfElements (int[] array)
+ {
+
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+        count++;
+        }
+ }
+
+Console.WriteLine($"Количество четных элементов = {count}");
+}
+
+CounterOfElements(RandomArray(10));
+
